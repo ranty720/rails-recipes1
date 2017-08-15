b325a484ae2497399acfd3870146f43b8e2f8adc
ranty <ranty720@gmail.com>
2017-08-15 23:02:54 +0800

    UI

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,10 +1,27 @@
 class Admin::CategoriesController < ApplicationController
 
   def index
-    @categories = Category.all
-  end
-  def show
-    @category = Category.find(params[:id])
-  end
-  
+       @categories = category.all
+     end
+
+     def edit
+       @category = category.find(params[:id])
+     end
+
+     def update
+       @category = category.find(params[:id])
+
+       if @category.update(category_params)
+         redirect_to admin_categories_path
+       else
+         render "edit"
+       end
+     end
+
+     protected
+
+     def category_params
+       params.require(:category)
+     end
+
 end
