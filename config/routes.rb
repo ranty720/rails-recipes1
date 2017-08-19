4420e38562691fb697fc3ce7fefa0a146403c413
ranty <ranty720@gmail.com>
2017-08-19 20:32:19 +0800

    tag

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,14 @@ Rails.application.routes.draw do
     root "events#index"
     resources :events do
       resources :tickets, :controller => "event_tickets"
+      collection do
+        post :bulk_update
+      end
     end
     resources :users do
       resource :profile, :controller => "user_profiles"
+
+
     end
   end
 
